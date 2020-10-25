e801e1b77d78a39bb1624fa2406256e35cd27674
xilingdc <55708629+xilingdc@users.noreply.github.com>
2020-10-25 19:08:56 -0400

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,23 +1,43 @@
 import java.util.ArrayList;
 import java.util.List;
 
+    /**
+    * @author Ali Fahd
+    */
+
 public class Player {
 
-    private String name;
-    private List<Country> countries = new ArrayList<>();
-
+    private String name;//player name
+    private List<Country> countries = new ArrayList<>();//player's country list
+    
+    
+    /**
+    * @Constructor
+    */
     public Player(String name) {
         this.name = name;
     }
-
+    
+    
+    /**
+    *@param c Country, add c into list
+    */
     public void addCountry(Country c){
         countries.add(c);
     }
-
+    
+    
+    /**
+    *@param c Country, remove c from list
+    */
     public void removeCountry(Country c){
         countries.remove(c);
     }
-
+    
+    
+    /**
+    *@param troops int
+    */
     public void distributeTroops(int troops){
         while(troops != 0){
             for (Country c: countries){
@@ -28,11 +48,19 @@ public class Player {
             }
         }
     }
-
+    
+    
+    /**
+    *@return player's name
+    */
     public String getName(){
         return name;
     }
-
+    
+    
+    /**
+    *@return player's country list
+    */
     public List<Country> getCountries(){
         return countries;
     }
