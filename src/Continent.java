13a88111804bef5a101706e09b9d16eb909ee857
Aleksandar Veselinovic <vesa.2k@gmail.com>
2020-12-09 21:45:00 -0500

    missing private

--- src/Continent.java
+++ src/Continent.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Continent {
     private String name;
     private ArrayList<Country> countries;
-    int bonusTroops;
+    private int bonusTroops;
 
     /**
      * constructor
